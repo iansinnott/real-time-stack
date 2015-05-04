7337351e6a7809029c830b26df26c64de267fb02
Ian Sinnott <ian@iansinnott.com>
2015-05-03 22:45:52 -0700

    Update router to pass params

--- client/index.js
+++ client/index.js
@@ -1,18 +1,19 @@
-'use strict';
-
 /**
  * Entry point for the entire app. Currently this is also where we keep the
  * Router.
  */
 
-var React  = require('react'),
-    Router = require('react-router');
+var React  = require('react');
+var Router = require('react-router');
 
-var routes = require('./Router');
+var routes = require('./routes.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-  Router.run(routes, Handler => {
-    React.render(<Handler/>, document.getElementById('root'));
+  Router.run(routes, (Handler, state) => {
+    React.render(
+      <Handler params={state.params} />,
+      document.body
+    );
   });
 });
 
