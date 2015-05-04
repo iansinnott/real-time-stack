bfc5b1d615ee85f91b206e7ba4c9d4fa0f0c3ff3
Ian Sinnott <ian@iansinnott.com>
2015-05-03 22:46:18 -0700

    Add clean and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,43 @@
 'use strict';
 
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var fs = require('fs');
 
-    // Nodemon
-    nodemon = require('nodemon'),
+// Nodemon
+var nodemon = require('nodemon');
 
-    // Webpack
-    webpack = require('webpack'),
-    WebpackDevServer = require('webpack-dev-server');
+// Webpack
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
 
-var config = require('./webpack.config');
+var config = require('./webpack.config.js');
+var devConfig = require('./webpack.dev.config.js');
+
+/**
+ * Build for production. We use an empty function b/c we don't care what happens
+ * with the build. The arguments are included for reference in case we want to
+ * use them later on. For full docs see:
+ * http://webpack.github.io/docs/node.js-api.html
+ *
+ * Note: We can remove the eslint declarations if we handle the error.
+ */
+gulp.task('build', function() {
+  /* eslint-disable */
+  webpack(config, function(err, stats){ /* Empty function */ });
+  /* eslint-enable */
+});
+
+/**
+ * Remove bundled files from public/
+ * Note: This does not remove the public directory itself
+ */
+gulp.task('clean', function() {
+  fs.readdirSync('public').forEach(function(filename) {
+    fs.unlinkSync('public/' + filename);
+  });
+  console.log(gutil.colors.green('`public/` directory cleaned.'));
+});
 
 gulp.task('nodemon', function() {
   nodemon({
@@ -25,9 +52,9 @@ gulp.task('nodemon', function() {
 gulp.task('webpack', function() {
   var DEV_PORT = process.env.DEV_PORT || 8080;
 
-  var server = new WebpackDevServer(webpack(config), {
+  var server = new WebpackDevServer(webpack(devConfig), {
     contentBase: './public/',
-    publicPath: config.output.publicPath,
+    publicPath: devConfig.output.publicPath,
     hot: true,
     inline: true,
     stats: {
